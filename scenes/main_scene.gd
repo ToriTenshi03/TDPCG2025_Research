5b2ecd2319c8382bd4b77fc66e8302d45c702545
Nameless Tenshi <syahmiichiro03@gmail.com>
2026-01-07 21:24:29 +0800

    Auto set max tiles

--- scenes/main_scene.gd
+++ scenes/main_scene.gd
@@ -2,9 +2,13 @@ class_name WorldLayer
 extends TileMapLayer
 
 var IDCounter:int = 1
-var maxIDCounter:int = 2
+var maxIDCounter:int = -1
+
 
 signal IDCounterChanged(id:int)
+
+func _init() -> void:
+	_setMaxTiles()
 
 func _input(event: InputEvent) -> void:
 	var tileCoords = local_to_map(get_global_mouse_position())
@@ -17,3 +21,12 @@ func _input(event: InputEvent) -> void:
 			set_cell(tileCoords,1,Vector2i(0,0),IDCounter)
 		elif event.button_index == MOUSE_BUTTON_RIGHT and event.pressed:
 			set_cell(tileCoords,1,Vector2i(0,0),-1)
+
+
+func _setMaxTiles():
+	set_cell(Vector2i(0,0),1,Vector2i(0,0),1)
+	var source_id = get_cell_source_id(Vector2i(0, 0))
+	if source_id > -1:
+		var scene_source = tile_set.get_source(source_id)
+		maxIDCounter = scene_source.get_scene_tiles_count()
+	set_cell(Vector2i(0,0),1,Vector2i(0,0),-1)
